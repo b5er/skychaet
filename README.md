f2a35fa39e1b2590c6d254c3b49864f7139edeb2
Brian Servia <bservia@gmail.com>
2019-11-29 00:03:59 -0800

    Small update to readme. Boilerplate yew (for now).

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # skychaet
 Chat Rust Frontend
 
+# Getting Started
+> cargo web start --release
+
 # Contributors
--- sky/src/main.rs
+++ sky/src/main.rs
@@ -1,3 +1,34 @@
+use yew::{html, Component, ComponentLink, Html, ShouldRender};
+
+struct Model {}
+
+enum Msg {
+	DoIt,
+}
+
+impl Component for Model {
+	type Message = Msg;
+	type Properties = ();
+
+	fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+		Model {}
+	}
+
+	fn update(&mut self, msg: Self::Message) -> ShouldRender {
+		match msg {
+			Msg::DoIt => {
+				true
+			}
+		}
+	}
+
+	fn view(&self) -> Html<Self> {
+		html! {
+			<button onclick=|_| Msg::DoIt>{ "Clicfk me!" }</button>
+		}
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	yew::start_app::<Model>();
 }
